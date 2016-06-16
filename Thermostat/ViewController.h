ba1e509a6d87215096d34b7d80b5d1e1b511cc6e
runner150 <kyleettinger@kyles-macbook-pro.local>
2016-06-16 12:56:03 -0500

    Initial Commit

---
+++ Thermostat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thermostat
+//
+//  Created by Kyle Ettinger on 6/16/16.
+//  Copyright © 2016 Kyle Ettinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
